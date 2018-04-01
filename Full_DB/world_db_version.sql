8fe67bee6d4d30fdc00677d7d6893b345e2bd626
Zyres <mr@eichbuehlerhof.de>
2018-04-01 02:05:23 +0200

    Add id (auto increment) to table world_db_version

--- Full_DB/world_db_version.sql
+++ Full_DB/world_db_version.sql
@@ -5,14 +5,16 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 CREATE TABLE IF NOT EXISTS `world_db_version` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `LastUpdate` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`LastUpdate`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 DELETE FROM `world_db_version`;
 /*!40000 ALTER TABLE `world_db_version` DISABLE KEYS */;
-INSERT INTO `world_db_version` (`LastUpdate`) VALUES
-	('1.3.18_build_creature_properties');
+INSERT INTO `world_db_version` (`id`, `LastUpdate`) VALUES
+	(1, '1.3.18_build_creature_properties'),
+	(2, '1.3.19_world_db_version');
 /*!40000 ALTER TABLE `world_db_version` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
