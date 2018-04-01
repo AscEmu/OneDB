a7131aa646b36dbd80bc3f98a88fb8a6c74fb2a9
Zyres <mr@eichbuehlerhof.de>
2018-04-01 15:31:09 +0200

    Use at least ae build version in creature_properties

--- Full_DB/world_db_version.sql
+++ Full_DB/world_db_version.sql
@@ -8,14 +8,15 @@ CREATE TABLE IF NOT EXISTS `world_db_version` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `LastUpdate` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 DELETE FROM `world_db_version`;
 /*!40000 ALTER TABLE `world_db_version` DISABLE KEYS */;
 INSERT INTO `world_db_version` (`id`, `LastUpdate`) VALUES
 	(1, '1.3.18_build_creature_properties'),
 	(2, '1.3.19_world_db_version'),
-	(3, '1.3.20_build_player_xp_for_level');
+	(3, '1.3.20_build_player_xp_for_level'),
+	(4, '1.3.21_build_creature_properties');
 /*!40000 ALTER TABLE `world_db_version` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
