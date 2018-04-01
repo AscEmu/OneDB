3a101894e431ec4abf03e0d21fee51bcd9ba5975
Zyres <mr@eichbuehlerhof.de>
2018-04-01 02:42:41 +0200

    Multiversion definitions for player_xp_for_level

--- Full_DB/world_db_version.sql
+++ Full_DB/world_db_version.sql
@@ -8,13 +8,14 @@ CREATE TABLE IF NOT EXISTS `world_db_version` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `LastUpdate` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 DELETE FROM `world_db_version`;
 /*!40000 ALTER TABLE `world_db_version` DISABLE KEYS */;
 INSERT INTO `world_db_version` (`id`, `LastUpdate`) VALUES
 	(1, '1.3.18_build_creature_properties'),
-	(2, '1.3.19_world_db_version');
+	(2, '1.3.19_world_db_version'),
+	(3, '1.3.20_build_player_xp_for_level');
 /*!40000 ALTER TABLE `world_db_version` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
