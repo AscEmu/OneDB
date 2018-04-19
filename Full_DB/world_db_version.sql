7d07c9fe605f70ca56e8d26fe55b60b62a557506
Zyres <mr@eichbuehlerhof.de>
2018-04-19 18:56:41 +0200

    Add language/racial skill to worgen and goblins.

--- Full_DB/world_db_version.sql
+++ Full_DB/world_db_version.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `world_db_version` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `LastUpdate` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 DELETE FROM `world_db_version`;
 /*!40000 ALTER TABLE `world_db_version` DISABLE KEYS */;
@@ -31,7 +31,8 @@ INSERT INTO `world_db_version` (`id`, `LastUpdate`) VALUES
 	(16, '1.3.33_playercreateinfo_misc'),
 	(17, '1.3.34_playercreateinfo_introid'),
 	(18, '1.3.35_playercreateinfo_faction'),
-	(19, '1.3.36_playercreateinfo_displayid');
+	(19, '1.3.36_playercreateinfo_displayid'),
+	(20, '1.3.37_worgen_goblin_language');
 /*!40000 ALTER TABLE `world_db_version` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
